696f2ec2f5a327c1dde5c21f9a95b3025366bf76
jss <jss@jssserver>
2015-12-30 20:57:35 +0800

    Add log output function.

--- example/src/main/java/com/jss/test/controller/IndexController.java
+++ example/src/main/java/com/jss/test/controller/IndexController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
